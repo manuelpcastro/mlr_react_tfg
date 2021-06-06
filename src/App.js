8df8538b6142ab2071809a81b3f611dcbc035332
LuisE_FernandezCurbelo <63299463+lefdez@users.noreply.github.com>
2021-06-06 23:50:12 +0200

    add auth component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import requireAuth from "./utils/RequireAuth";
 
 import Root from "./Root";
 import { Route, Switch } from "react-router-dom";
@@ -23,7 +24,7 @@ class App extends Component {
           <Switch>
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
-            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/dashboard" component={requireAuth(Dashboard)} />
             <Route exact path="/" component={Home} />
             <Route path="*">Ups</Route>
           </Switch>
