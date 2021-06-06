58b732a5b26f59cbdc5d3669679fac0ca2aa1795
LuisE_FernandezCurbelo <63299463+lefdez@users.noreply.github.com>
2021-06-06 21:09:31 +0200

    add signup and login components

--- src/App.js
+++ src/App.js
@@ -3,16 +3,40 @@ import './App.css';
 import React, { Component, Fragment } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+
+import Root from "./Root";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Header />
-        <Home />
-      </Fragment>
+      <div>
+        <Root>
+          <Switch>
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route exact path="/" component={Home} />
+            <Route path="*">Ups</Route>
+          </Switch>
+        </Root>
+      </div>
     );
   }
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Fragment>
+//         <Header />
+//         <Home />
+//       </Fragment>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
 
 import axios from "axios";
@@ -8,43 +9,61 @@ import {API_MLRMODEL_URL} from "../constants";
 import NewMlrModelModal from "./NewMlrModelModal";
 import MlrModelList from "./MlrModelList";
 
-
 class Home extends Component {
-  state = {
-    mlr_models: []
-  };
-
-  componentDidMount() {
-    this.resetState();
-  }
-
-  getMlrModels = () => {
-    axios.get(API_MLRMODEL_URL).then(res => this.setState({ mlr_models: res.data }));
-  };
-
-  resetState = () => {
-    this.getMlrModels();
-  };
-
   render() {
     return (
-      <Container style={{ marginTop: "20px" }}>
-        <Row>
-          <Col>
-            <MlrModelList
-              mlr_models={this.state.mlr_models}
-              resetState={this.resetState}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <NewMlrModelModal create={true} resetState={this.resetState} />
-          </Col>
-        </Row>
+      <Container>
+        <h1>Home</h1>
+        <p>
+          <Link to="/login/">Login</Link>
+        </p>
+        <p>
+          <Link to="/signup">Sign up</Link>
+        </p>
+        <p>
+          <Link to="/dashboard">Dashboard</Link>
+        </p>
       </Container>
     );
   }
 }
+
+// class Home extends Component {
+//   state = {
+//     mlr_models: []
+//   };
+//
+//   componentDidMount() {
+//     this.resetState();
+//   }
+//
+//   getMlrModels = () => {
+//     axios.get(API_MLRMODEL_URL).then(res => this.setState({ mlr_models: res.data }));
+//   };
+//
+//   resetState = () => {
+//     this.getMlrModels();
+//   };
+//
+//   render() {
+//     return (
+//       <Container style={{ marginTop: "20px" }}>
+//         <Row>
+//           <Col>
+//             <MlrModelList
+//               mlr_models={this.state.mlr_models}
+//               resetState={this.resetState}
+//             />
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col>
+//             <NewMlrModelModal create={true} resetState={this.resetState} />
+//           </Col>
+//         </Row>
+//       </Container>
+//     );
+//   }
+// }
 
 export default Home;
