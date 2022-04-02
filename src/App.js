27d7ebb756f7391784c7b511d0ab01a57a08c5d1
manuelpcastro <manuelpancorbocastro@gmail.com>
2022-04-02 16:59:57 +0200

    Update missing import updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import "./App.css"
 
-import React, { Component, Fragment } from "react"
-import { Route, Switch } from "react-router-dom"
+import React, { Component } from "react"
 import { ToastContainer } from "react-toastify"
 import axios from "axios"
-import Home from "./components/Home"
-import Signup from "./components/signup/Signup"
-import Login from "./components/login/Login"
-import Dashboard from "./components/dashboard/Dashboard"
-
 import Root from "./Root"
 import { API_URL } from "./constants"
-import Router from "./Router"
+import Router from "./routes/Router"
 
 axios.defaults.baseURL = API_URL
 
