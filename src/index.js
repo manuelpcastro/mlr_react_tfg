de98d5902b0436ba06ec939fdfadd0759881340d
manuelpcastro <manuelpancorbocastro@gmail.com>
2022-04-03 16:52:39 +0200

    Add custom css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
-import "bootstrap/dist/css/bootstrap.css"
+import "./css/site.scss"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "react-toastify/dist/ReactToastify.css"
 
