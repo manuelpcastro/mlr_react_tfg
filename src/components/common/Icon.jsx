0bf02895629e45c51d85f2358b93704c67e9f428
manuelpcastro <manuelpancorbocastro@gmail.com>
2022-05-10 20:13:44 +0200

    Add logout action

--- src/components/common/Icon.jsx
+++ src/components/common/Icon.jsx
@@ -1,20 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const Icon = ({ className, icon, size }) => {
+const Icon = ({
+  className, icon, size, onClick,
+}) => {
   const sizeClass = size ? `fa-${size}` : ""
-  return <i className={`fa fa-${icon} ${sizeClass} ${className}`} />
+  return <i onClick={onClick} className={`fa fa-${icon} ${sizeClass} ${className}`} />
 }
 
 Icon.propTypes = {
   className: PropTypes.string,
   icon: PropTypes.string.isRequired,
   size: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 Icon.defaultProps = {
   className: "",
   size: "",
+  onClick: () => {},
 }
 
 export default Icon
