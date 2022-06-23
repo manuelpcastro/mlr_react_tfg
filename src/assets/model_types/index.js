1fde469832ac3674d73f4359f536634c46d39e5f
manuelpcastro <manuelpancorbocastro@gmail.com>
2022-06-23 19:35:57 +0200

    Add model type icons

---
+++ src/assets/model_types/index.js
@@ -0,0 +1,27 @@
+import NN from "./nn_black.svg"
+import ColorNN from "./nn_color.svg"
+import RandomForest from "./randomforest_black.svg"
+import ColorRandomForest from "./randomforest_color.svg"
+import SVM from "./svm_black.svg"
+import ColorSVM from "./svm_color.svg"
+import DecisionTree from "./decisiontree_black.svg"
+import ColorDecisionTree from "./decisiontree_color.svg"
+import XGBoost from "./xgboost_black.svg"
+import ColorXGBoost from "./xgboost_color.svg"
+
+// TO DO: take these images from the backend
+// what if a new type is added
+const modelsIcons = {
+  NN,
+  ColorNN,
+  RandomForest,
+  ColorRandomForest,
+  SVM,
+  ColorSVM,
+  DecisionTree,
+  ColorDecisionTree,
+  XGBoost,
+  ColorXGBoost,
+}
+
+export default modelsIcons
